08ca31f7a0fb81f7d77cf0b9bc0aa589974e7093
Chachigo <cguilbaud1@gmail.com>
2024-10-10 18:53:51 +0200

    fix epic games plugin and add force_epic command

--- plugins/free_epicgames.py
+++ plugins/free_epicgames.py
@@ -1,5 +1,6 @@
 from interactions import *
 from datetime import datetime
+from interactions.ext.prefixed_commands import prefixed_command, PrefixedContext
 import requests
 import json
 from config import *
@@ -8,11 +9,13 @@ class free_epicgames(Extension):
     def __init__(self,bot):
         print("Epic Games Plugin loaded")
 
+    force = False
+    
     @Task.create(IntervalTrigger(seconds=10))
     async def send_epic_free_games(self) -> None:
         now = datetime.now()
-        if now.weekday() == 3 and now.hour == 17 and now.minute == 15:
-            epic_games_channel = self.bot.fetch_channel(EPIC_CHANNEL_ID)
+        if self.force == True or now.weekday() == 3 and now.hour == 17 and now.minute == 15:
+            epic_games_channel = await self.bot.fetch_channel(EPIC_CHANNEL_ID)
             epic_url = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=fr&country=FR&allowCountries=FR"
             base_shop_url = "https://store.epicgames.com/fr/p/"
 
@@ -33,6 +36,15 @@ class free_epicgames(Extension):
                         game_epic_id = game["urlSlug"]
                     await epic_games_channel.send(base_shop_url + game_epic_id)
 
+    @prefixed_command(name="force_epic")
+    async def force_command(self,ctx: PrefixedContext)-> None:
+        if ctx.author_id == ADMIN_DISCORD_ID and ctx.guild is None:
+            self.force = True
+            await self.send_epic_free_games()
+            self.force = False
+            await self.bot.send_log_dm("Force epic")
+            return
+    
     @listen()
     async def on_startup(self):
         self.send_epic_free_games.start()
